81be502ce9da7843b0ca00746450bf40e9fa3ae5
abhishekkumar321 <53290881+abhishekkumar321@users.noreply.github.com>
2019-07-30 11:42:14 +0530

    Program 10.cpp

--- program 10.cpp
+++ program 10.cpp
@@ -3,27 +3,30 @@
 //program 10: Summation of primes
 
 #include<iostream>
-#include<cmath>
 using namespace std;
  
 int main()
 {
-		long long num,sum=0;
-		cin>>num;
-		for(long long i=2;i<=num;i++)
+		long long n,sum=0;
+		cin>>n;
+		for(long long i=2;i<=n;i++)
 		{
+			if(i%2==!0 || i==2)
+			{
 			int flag=0;
-			for(long long j=2;j<=sqrt(i);j++)
+			for(long long j=2;j*j<=i;j++)
 			if(i%j==0)
 			{flag=1;break;}
 			if(flag==0)
 			sum=sum+i;
-			
+			}
 			
 		}
 		
 cout<<sum; 	
 return 0;
 }
+	
+    
 	
     
