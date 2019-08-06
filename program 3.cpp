e020e804444f776c534797d3c915d58c772d402f
abhishekkumar321 <53290881+abhishekkumar321@users.noreply.github.com>
2019-08-06 12:04:58 +0530

    Update program 3.cpp

--- program 3.cpp
+++ program 3.cpp
@@ -1,37 +1,31 @@
 
-//Name: Abhishek Kumr
+//Name: Abhishek Kumar
 //registration number:1941012170
 //program 3: Largest prime factor
 
-#include<iostream>
-#include<cmath>
 
+#include<iostream>
 using namespace std;
- 
 int main()
 {
-		unsigned long long factor,num;
-		cin>>num;
-		
-		for(unsigned long long i=2;i<=num;i++)
-		{
-			if(i%2==!0)
-				{
-					int flag=0;
-					for(unsigned  long long j=2;j*j<=i;j++)
-					if(i%j==0)
-					{flag=1;break;}
-					if(flag==0 && num%i==0)
-					{factor=i;num=num/i;}
-				}
-			
-			
-			
-		}
-		
-cout<<factor; 	
-return 0;
-}
-	
-    
+    long long  num;
+    cin>>num;
+    long long n=2, max_fact;
+    while(num!=0)
+    {
+        if(num % n!=0)
+            ++n;
+        else
+        {
+            max_fact = num;
+            num = num / n;
+            if(num == 1)
+            {
+            cout<<max_fact;
+            break;
+            }
+        }
+    }
+    return 0;
+}    
     
