353e2ba2802976c61024c2292c29f2f4243ab017
abhishekkumar321 <53290881+abhishekkumar321@users.noreply.github.com>
2019-08-06 15:06:07 +0530

    Update program 10.cpp

--- program 10.cpp
+++ program 10.cpp
@@ -12,11 +12,10 @@ int main()
 		cin>>n;
 		for(long long i=2;i<=n;i++)
 		{
-			if(i%2==!0 || i==2 || i%3==!0 || i==3 || i%5==!0 || i==5 || i%7==!0 || i==7)
+			if(i%2==!0 || i==2)
 			{
 			int flag=0;
-			for(long long j=2;j<=sqrt(i);j++)
-			if(j%2==!0 || j==2 )
+			for(long long j=3;j<=sqrt(i);j=j+2)
 			if(i%j==0)
 			{flag=1;break;}
 			if(flag==0)
